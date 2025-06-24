6f44726f66df51fefcad8006659d4f17ab1ec4f6
Patrick Gross <bears_lair@protonmail.com>
2025-06-23 22:11:39 -0700

    imported routes and css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import "./App.css";
 import Home from "./pages/Home";
+import "./css/App.css";
 import Favorites from "./pages/Favorites";
 import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+// The app demonstrates changing pages while keeping the Navbar during page changes.
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
