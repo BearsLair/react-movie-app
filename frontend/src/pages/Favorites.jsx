dc7bd9dfe88a7d8701519870aefabb8f9d975743
Patrick Gross <bears_lair@protonmail.com>
2025-06-23 20:59:02 -0700

    empty favorites added to component

---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,9 @@
+function Favorite() {
+  return (
+    <div className="favorites-empty">
+      <p>Start adding movies to your favorites and they will appear here.</p>
+    </div>
+  );
+}
+
+export default Favorite;
